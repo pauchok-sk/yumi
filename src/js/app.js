d03e51092a7f2bbf9e94c54bdbb4af9dc71399e1
pauchok-sk <pauchoooooook@gmail.com>
2025-07-31 11:55:48 +0300

    animation

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import "../scss/style.scss";
+import loader from "./files/loader.js";
+import spoller from "./files/spoller.js";
+
+spoller();
+loader();
---
+++ src/js/files/loader.js
@@ -0,0 +1,31 @@
+export default function loader() {
+  const loader = document.querySelector(".loader");
+
+  if (loader) {
+    window.addEventListener("load", () => {
+      const cloudT = loader.querySelector(".loader__cloud-t");
+      const cloudBl = loader.querySelector(".loader__cloud-bl");
+      const cloudBr = loader.querySelector(".loader__cloud-br");
+
+      setTimeout(() => {
+        cloudT.style.opacity = 0;
+        cloudT.style.top = "-100%";
+
+        cloudBl.style.opacity = 0;
+        cloudBl.style.left = "-100%";
+
+        cloudBr.style.opacity = 0;
+        cloudBr.style.right = "-100%";
+
+        setTimeout(() => {
+          loader.style.opacity = 0;
+          loader.style.visibility = "hidden";
+
+          setTimeout(() => {
+            loader.remove();
+          }, 500);
+        }, 1800);
+      }, 300);
+    });
+  }
+}
